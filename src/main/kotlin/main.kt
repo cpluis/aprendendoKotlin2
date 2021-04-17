d08390d5ebd29c746d3c74816d7f150ba514dddb
luis.pires <luispires.m@gmail.com>
2021-04-16 23:14:45 -0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    //trabalhando com os laços de repetição while e for
+
+    var contador = 0
+
+    while (contador < 5){
+         println("Contador $contador")
+        contador = contador +1
+    }
+}
